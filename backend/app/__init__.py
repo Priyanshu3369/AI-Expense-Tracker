871df55f16b395790b84bd249a021f776c07d549
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-06-06 07:52:46 +0530

    added basic file and folder structure

---
+++ backend/app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, migrate, jwt, cors
+from .routes import register_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Init extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app)
+
+    # Register blueprints
+    register_routes(app)
+
+    return app
---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+def register_routes(app):
+    # Import and register Blueprints
+    pass  # Will add blueprints in upcoming steps
