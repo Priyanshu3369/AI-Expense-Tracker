a3572c69045b8201f3ed43c50b399b4a257f9e19
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-06-06 07:47:28 +0530

    starting fresh

--- backend/app/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-auth_bp = Blueprint("auth", __name__)
-
-@auth_bp.route("/test", methods=["GET"])
-def test():
-    return jsonify(message="Auth route working!")
