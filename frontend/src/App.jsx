60fde8554f9734645b8cd85de83e2f99a9c821bf
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-06-08 07:03:33 +0530

    add basic frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="h-screen bg-gradient-to-r from-indigo-500 to-purple-500 flex items-center justify-center text-white text-2xl font-bold">
+      Smart Expense Tracker Frontend Ready 🚀
+    </div>
+  )
+}
+export default App
